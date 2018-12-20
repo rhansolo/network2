9b351a1118a72698952105e22d671a3ff43fdbed
robinhanstuy <rhan@stuy.edu>
2018-12-20 02:10:53 -0500

    .

--- basic_server.c
+++ basic_server.c
@@ -1,10 +1,31 @@
 #include "pipe_networking.h"
 
+static void sighandler(int signo) {
+  if (signo == SIGINT) {
+    remove("wkp");
+    exit(1);
+  }
+}
 
 int main() {
+	
 
-  int to_client;
-  int from_client;
+	int to_client;
+	int from_client;
+	char in[BUFFER_SIZE];
+	signal(SIGINT, sighandler);
 
-  from_client = server_handshake( &to_client );
+	while (1)
+	{
+	    from_client = server_handshake( &to_client );
+	    while (read(from_client, in, sizeof(in)) ){
+	      	printf("[server] received message: %s", in);
+	      	char* tmp = in;
+	      	while (*tmp){
+		       *tmp = toupper((unsigned char) *tmp);
+	   			tmp++;
+	      	}
+	      	write(to_client, in, sizeof(in));
+   	 	}
+	}
 }
--- client.c
+++ client.c
@@ -7,4 +7,18 @@ int main() {
   int from_server;
 
   from_server = client_handshake( &to_server );
+  char in[BUFFER_SIZE];
+  char out[BUFFER_SIZE];
+
+  while (1){
+
+    printf("[client] enter a phrase: ");
+    fgets(in, sizeof(in), stdin);
+    write(to_server, in, sizeof(in));
+    read(from_server, out, sizeof(out));
+    printf("[client] returned response: %s\n", out);
+
+  }
+
+  return 0;
 }
